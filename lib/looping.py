907dd20136200ff858b4e380fa4f9ce64c1fd205
Nahason <nahason.murithi@student.moringaschool.com>
2023-11-25 19:25:14 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        if i == 0:
+            print("Happy New Year!")
+        else:
+            print (i)
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squaredResult = [num ** 2 for num in int_list]
+    return squaredResult
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while num <= 100:
+        if num % 3*5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+      
+fizzbuzz()
